1685aa8d72f168db47627bd734f269ef41dc331b
Stranger <497004982@qq.com>
2019-01-02 23:30:33 +0800

    代理池

---
+++ proxy_pool-master/Run/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from multiprocessing import Process
+
+sys.path.append('../')
+
+from Api.ProxyApi import run as ProxyApiRun
+from Schedule.ProxyValidSchedule import run as ValidRun
+from Schedule.ProxyRefreshSchedule import run as RefreshRun
+
+
+def run():
+    p_list = list()
+    p1 = Process(target=ProxyApiRun, name='ProxyApiRun')
+    p_list.append(p1)
+    p2 = Process(target=ValidRun, name='ValidRun')
+    p_list.append(p2)
+    p3 = Process(target=RefreshRun, name='RefreshRun')
+    p_list.append(p3)
+
+    for p in p_list:
+        p.daemon = True
+        p.start()
+    for p in p_list:
+        p.join()
+
+
+if __name__ == '__main__':
+    run()
